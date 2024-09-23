007c6fe7d5c2defaedd5b0efe6eb5471ff89affa
Yeming Hu <yeminghuhym@gmail.com>
2024-09-22 21:52:28 -0400

    modified server.js

--- server.js
+++ server.js
@@ -16,7 +16,10 @@ const app = express();
 
 // Middleware
 // app.use(cors());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+// app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({ origin: "https://ims-frontend-tawny.vercel.app", credentials: true })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
